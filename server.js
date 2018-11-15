4ec018446e3ed2ebbe3e643afe3a6e4df439c1e2
Mark Lewin <mark.lewin@vonage.com>
2018-11-15 12:01:23 +0000

    Complete ES6 refactoring

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app
     .get(handleInboundSms)
     .post(handleInboundSms);
 
-let concat_sms = []; // Array of message objects
+const concat_sms = []; // Array of message objects
 
-function handleInboundSms(request, response) {
+const handleInboundSms = (request, response) => {
     const params = Object.assign(request.query, request.body);
-
-    if (params['concat'] === 'true') {
+    console.dir(params)
+    if (params['concat'] == 'true') {
         /* This is a concatenated message. Add it to an array
            so that we can process it later. */
         concat_sms.push({
@@ -27,9 +27,7 @@ function handleInboundSms(request, response) {
 
         /* Do we have all the message parts yet? They might
            not arrive consecutively. */
-        let parts_for_ref = concat_sms.filter(function (part) {
-            return part.ref == params['concat-ref'];
-        });
+        const parts_for_ref = concat_sms.filter(part => part.ref == params['concat-ref']);
 
         // Is this the last message part for this reference?
         if (parts_for_ref.length == params['concat-total']) {
@@ -45,20 +43,13 @@ function handleInboundSms(request, response) {
     response.status(204).send();
 }
 
-function processConcatSms(all_parts) {
+const processConcatSms = (all_parts) => {
 
-    // Order all the message parts
-    all_parts.sort(function (a, b) {
-        if (Number(a.part) < Number(b.part)) {
-            return -1;
-        } else {
-            return 1;
-        }
-    })
-
-    let concat_message = '';
+    // Sort the message parts
+    all_parts.sort((a, b) => a.part - b.part);
 
     // Reassemble the message from the parts
+    let concat_message = '';
     for (i = 0; i < all_parts.length; i++) {
         concat_message += all_parts[i].message;
     }
@@ -66,7 +57,7 @@ function processConcatSms(all_parts) {
     displaySms(all_parts[0].from, concat_message);
 }
 
-function displaySms(msisdn, text) {
+const displaySms = (msisdn, text) => {
     console.log('FROM: ' + msisdn);
     console.log('MESSAGE: ' + text);
     console.log('---');
